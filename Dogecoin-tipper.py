b891e8ada204018a0a1cd9a1def3e45778d6e5e5
kaare8p <kaare1994@gmail.com>
2014-05-18 02:08:56 +0200

    Minor changes

--- Dogecoin-tipper.py
+++ Dogecoin-tipper.py
@@ -6,16 +6,24 @@ user_agent = ("Dogecoin tipper 1.0 by /u/kaare8p"
 		      "github.com/kaare8p/Dogecoin-tipper")
 r = praw.Reddit(user_agent=user_agent)
 
-user = 'kaare8p'	
+user = 'DogeCoinTipperb'	
 r.login(user, '')
 
 already_done = set()
-prawUsers =[user, 'dogetipbot', 'dogetipchecker']
+prawUsers =[user, 'dogetipbot', 'dogetipchecker', 'changetip', 'Dogeseedbot', 'Randomactofdogebot']
+prawWords =[":(", "):", ":-(", ")-:", ":'(", ":|"]
 
-subreddit = r.get_subreddit('rbottesting121')
+subreddit = r.get_subreddit('dogecoin')
+
 counter = 0
 counter2 = 0
-amount = 5
+
+amount = 9.8
+comment_text = ("You seem sad, have some doge!\n\n"
+		"+/u/dogetipbot %.1f doge\n\n"
+		"Sorry for the small amount, every sad shibe have to get some!"
+		"^^Please ^^consider ^^donating ^^to ^^keep ^^me ^^running!"
+		"[^^Github:](https://github.com/kaare8p/Dogecoin-tipper)" % amount)
 
 def ratelimit(func, *args, **kwargs):
 	while True:
@@ -42,30 +50,33 @@ def check_balance():
 				s=''.join(i for i in op_line if i.isdigit() or i == ".")
 				return float(s)
 
-balance = check_balance()
+balance = 134.6#check_balance()
 
 while True:
 	comments = subreddit.get_comments(limit = 100)
 	for comment in comments:
-
+		
+		counter += 1
 		author = comment.author
-		if author is None:
-			has_praw = True
-		else:
-			has_praw = any(string in author.name for string in prawUsers)
+		op_text = comment.body.lower()
 
-		if comment.id not in already_done and not has_praw and balance >= amount:
-			counter += 1
-			ratelimit(comment.reply, "I'm testing my Tipping bot, have some Doge!\n\n+/u/dogetipbot %d doge" % amount)
+		if author is None:
+			has_praw_users = True
+		else:
+			has_praw_users = any(string in author.name for string in prawUsers)
+		has_praw = any(string in op_text for string in prawWords)
+
+		if comment.id not in already_done and not has_praw_users and has_praw and balance >= amount:
+			ratelimit(comment.reply, comment_text)
 			already_done.add(comment.id)
 			print ("Number %d" % counter)
 			balance -= amount
 
-	if (counter - counter2 > 25):
+	if (counter - counter2 > 100):
 		counter2 = counter
 		balance = check_balance()
 		if balance < amount:
-			print "\tExiting due to lack of funds")
+			print ("\tExiting due to lack of funds")
 			exit()
 	print ("\tSleeping for 10 seconds")	
 	time.sleep(10)
