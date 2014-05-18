9b3151d69426451c502bf15a387a666332896978
kaare8p <kaare1994@gmail.com>
2014-05-18 16:47:54 +0200

    Rewrote the code a bit, and added a thank you to users for donating.

--- Dogecoin-tipper.py
+++ Dogecoin-tipper.py
@@ -2,29 +2,30 @@ import praw
 import time
 import logging
 
+# Basic configuration
 logging.basicConfig(filename='Dogecoin-tipper.log',level=logging.DEBUG)
 
 user_agent = ("Dogecoin tipper 1.0 by /u/kaare8p"
 		      "github.com/kaare8p/Dogecoin-tipper")
 r = praw.Reddit(user_agent=user_agent)
-
 user = 'DogeCoinTipperb'	
-r.login(user, '')
+r.login(user, 'dogecoin1221')
 
 already_done = set()
-prawUsers =[user, 'dogetipbot', 'dogetipchecker', 'changetip', 'Dogeseedbot', 'Randomactofdogebot', 'TweetPoster']
+# Avoid comments from these users
+prawUsers =[user, 'dogetipbot', 'dogetipchecker', 'changetip', 'Dogeseedbot', 'Randomactofdogebot', 'TweetPoster', 'DogeHelpBot', 'DogeHelpBot']
 prawWords =[":(", ":-(", ":'(", ":|"]
 
 subreddit = r.get_subreddit('dogecoin')
 
 counter = 0
 amount = 9.8
-
 comment_text = ("You seem sad, have some doge!\n\n"
 		"+/u/dogetipbot %.1f doge\n\n"
 		"Sorry for the small amount, every sad shibe have to get some!\n\n"
 		"^^I'm ^^a ^^bot ^^built ^^for ^^sad ^^shibes. ^^Please ^^consider ^^donating ^^to ^^keep ^^me ^^running!\n" % amount)
 
+# Ensures compliance with reddit api rules
 def ratelimit(func, *args, **kwargs):
 	while True:
 		try:
@@ -35,18 +36,42 @@ def ratelimit(func, *args, **kwargs):
 			logging.info("\tSleeping for %d seconds" % error.sleep_time)
 			time.sleep(error.sleep_time)
 
+# Check how many doge is left on the bots account
 def check_balance():
 	prawTerms = ['+tip sent']
-	while True:
-		messages = r.get_inbox('comments')
-		for message in messages:
-			op_subject = message.subject
-			has_praw = any(string in op_subject for string in prawTerms)
-			if has_praw:
-				op_text = message.body
-				op_line = op_text.splitlines()[2]
-				s=''.join(i for i in op_line if i.isdigit() or i == ".")
-				return float(s)
+	messages = r.get_inbox('comments')
+	
+	for message in messages:
+		print message
+		op_subject = message.subject
+		has_praw = any(string in op_subject for string in prawTerms)
+		
+		if has_praw:
+			op_text = message.body
+			op_line = op_text.splitlines()[2]
+			s=''.join(i for i in op_line if i.isdigit() or i == ".")
+			
+			return float(s)
+
+# Check for donations, and thanks if any is present
+def check_tips():
+	prawTerms = ['+/u/dogetipbot']
+	messages = r.get_unread('comments')
+	
+	for message in messages:
+		op_text = message.body
+		author = message.author
+
+		has_praw = any(string in op_text for string in prawTerms)
+				
+
+		if has_praw and author.name != 'dogetipbot':
+			already_done.add(message.id)
+			ratelimit(message.reply, 'Thank you for tipping! This will help me cheer up other shibes! Much generous')
+			
+			print ("Posting reply to a donation")
+			logging.info("Posting reply to a donation")
+	return
 
 balance = check_balance()
 tips = balance/amount
@@ -54,8 +79,10 @@ tips = balance/amount
 print ("\tEnough Doge for %.0f tips" % tips)
 logging.info("\tEnough Doge for %.0f tips" % tips)
 
+# Main loop
 while True:
 	comments = subreddit.get_comments(limit = 100)
+	# Check for sad comments, and tip 'amoun' doge if found
 	for comment in comments:
 		
 		counter += 1
@@ -77,12 +104,14 @@ while True:
 			print ("Posted comment. Balance: %.1f Enough for %.0f tips" % (balance, tips))
 			logging.info("Posted comment. Balance: %.1f Enough for %.0f tips" % (balance, tips))
 
+	# If 500 or more comments parsed, check the balance to account for tips
 	if (counter > 500):
-		print "\tChecking balance..."
-		logging.info("\tChecking balance...")
+		print "\tChecking balance and tips..."
+		logging.info("\tChecking balance and tips...")
 		
 		counter = 0
 		time.sleep(20)
+		check_tip()
 		balance = check_balance()
 		
 		if balance < amount:
@@ -93,7 +122,7 @@ while True:
 		tips = balance/amount
 		print ("\tEnough Doge for %.0f tips" % tips)
 		logging.info("\tEnough Doge for %.0f tips" % tips)
-
-	print ("\tSleeping for 100 seconds")
-	logging.info("\tSleeping for 100 seconds")
+	
+	print ("\tSleeping for 180 seconds")
+	logging.info("\tSleeping for 180 seconds")
 	time.sleep(100)
