48623ecd332113d590644b871086715fb49ac116
kaare8p <kaare1994@gmail.com>
2014-06-13 18:49:55 +0200

    Changed error handling, and changed check_donations to not only read unread messages

--- Dogecoin-tipper.py
+++ Dogecoin-tipper.py
@@ -24,13 +24,31 @@ subreddit = r.get_subreddit('dogecoin')
 
 counter = 0
 
-#Log to file while printing to console
+# Log to file while printing to console
 def log(level, msg):
 	print("%s:  %s" % (time.ctime(), msg))
 	if level == 'warning':
 		logging.warning("%s:  %s" % (time.ctime(), msg))
 	else:
 		logging.info("%s:  %s" % (time.ctime(), msg))
+
+# Function for api-calls
+def api(level, func, *args, **kwargs):
+	while True:
+		try:
+			return func(*args, **kwargs)
+			break
+		except (HTTPError, ConnectionError, RedirectException, Timeout, timeout) as error:
+			log('warning', "%s::Reddit is down (%s): Sleeping" % (level, error))
+			time.sleep(30)
+			pass
+		except RateLimitExceeded as error:
+	                log('warning', "%s::RateLimitExceeded, sleeping %d seocnds" % (level, error.sleep_time))
+	                time.sleep(error.sleep_time)
+	                pass
+		except Exception as error:
+			log('warning', "%s::Error: %s" % (level, error))
+			raise
 
 # Calculate the tip
 def calculate_tip(balance):
@@ -43,58 +61,36 @@ def calculate_tip(balance):
 
 # Check how many doge is left on the bots account
 def check_balance():
-	result = None
-	while result is None:
-	        try:
-			if randint(1,4) == 4:
-				r.send_message('dogetipbot', 'history', '+history')
-	                messages = r.get_inbox(limit = 100)
-
-        	        for message in messages:
-                	        op_subject = message.subject
+	if randint(1,4) == 4:
+		api('check_balance()', r.send_message, 'dogetipbot', 'history', '+history')
+        messages = api('check_balance()', r.get_inbox)
+	
+	for message in messages:
+		op_subject = message.subject
                         
-	                        if op_subject == 're: history':
-        	                        op_text = message.body
-                	                op_line = op_text.splitlines()[3]
-                        	        result=''.join(i for i in op_line if i.isdigit() or i == ".")
+		if op_subject == 're: history':
+      			op_text = message.body
+              		op_line = op_text.splitlines()[3]
+	                result=''.join(i for i in op_line if i.isdigit() or i == ".")
                                 
-	                                return float(result)
-	        except (HTTPError, ConnectionError, RedirectException, Timeout, RateLimitExceeded, timeout) as error:
-			log('warning', "Check_balance()::Reddit is down (%s): Sleeping" % error)
-			time.sleep(30)
-			pass
-		except Exception as error:
-			log('warning', "Check_balance()::Error: %s" % error)
-			raise
+		        return float(result)
 
 # Check for donations, and thanks if any is present
 def check_donations():
-        try:
-                prawTerms = ['+/u/dogetipbot']
-                messages = r.get_unread('comments')
+	prawTerms = ['+/u/dogetipbot']
+        messages = api('check_donations()', r.get_inbox)
                 
-                for message in messages:
-                        op_text = message.body
-                        author = message.author
+        for message in messages:
+		op_text = message.body
+		author = message.author
 
-                        has_praw = any(string in op_text for string in prawTerms)
-                        if has_praw and author.name != 'dogetipbot' and message.id not in open('already_done.txt').read():
-                                with open('already_done.txt', 'a') as already_done:
-                                        already_done.write("%s\n" % message.id)
-                                message.reply('Thank you for tipping! This will help me cheer up other shibes, and will raise the amount i tip! very generosity')
+                has_praw = any(string in op_text for string in prawTerms)
+                if has_praw and author.name != 'dogetipbot' and message.id not in open('already_done.txt').read():
+	                with open('already_done.txt', 'a') as already_done:
+				already_done.write("%s\n" % message.id)
+                	api('check_donations', message.reply, 'Thank you for tipping! This will help me cheer up other shibes, and will raise the amount i tip! very generosity')
                                 
-                                log('info', "Posted reply to a donation")
-        except (HTTPError, ConnectionError, RedirectException, Timeout, timeout) as error:
-		log('warning', "Check_donations()::Reddit is down (%s): Sleeping" % error)
-		time.sleep(30)
-		pass
-	except RateLimitExceeded as error:
-                log('warning', "Check_donations()::RateLimitExceeded, sleeping %d seocnds" % error.sleep_time)
-                time.sleep(error.sleep_time)
-                pass
-	except Exception as error:
-		log('warning', "Check_donations()::Error: %s" % error)
-		raise
+                       	log('info', "Posted reply to a donation")
 	return
 
 
@@ -114,19 +110,11 @@ def tips_remaining(balance):
 
 #Do not tip replies to own comments
 def check_parent(parent_id, link_id):
-        try:
-                if parent_id != link_id:
-                        parent = r.get_info(thing_id=parent_id)
-                        author = parent.author
-                        if author.name == user:
-                                return True
-        except (HTTPError, ConnectionError, Timeout, RateLimitExceeded, RedirectException, timeout) as error:
-		log('warning', "Check_parent()::Reddit is down (%s): Sleeping" % error)
-		time.sleep(30)
-		pass
-	except Exception as error:
-		log('warning', "Check_parent()::Error: %s" % error)
-		raise
+	if parent_id != link_id:
+        	parent = api('check_parent', r.get_info, thing_id=parent_id)
+                author = parent.author
+                if author.name == user:
+	                return True
 	return False
 
 balance = check_balance()
@@ -143,67 +131,55 @@ log('info', "\tEnough Doge for %.0f tips" % tips)
 
 # Main loop
 while True:
-	try:
-		comments = subreddit.get_comments(limit = 300)
-		# Check for sad comments, and tip 'amount' doge if found
-		for comment in comments:
-			counter += 1
-			author = comment.author
-			op_text = comment.body.lower()
+	comments = api('Main', subreddit.get_comments, limit = 300)
+	# Check for sad comments, and tip 'amount' doge if found
+	for comment in comments:
+		counter += 1
+		author = comment.author
+		op_text = comment.body.lower()
 
-			if author is None:
-				has_praw_users = True
+		if author is None:
+			has_praw_users = True
+		else:
+			has_praw_users = any(string in author.name for string in prawUsers)
+		has_praw = any(string in op_text for string in prawWords)
+
+		if not has_praw_users and has_praw and balance >= amount and comment.id not in open('already_done.txt').read():
+			if author.name in open('userlist.txt').read():
+				log('info', "User %s have already received tip!" % author.name)
+
+			elif check_parent(comment.parent_id, comment.link_id):
+				log('info', "User %s commented a comment from the bot!" % author.name) 
+
 			else:
-				has_praw_users = any(string in author.name for string in prawUsers)
-			has_praw = any(string in op_text for string in prawWords)
+				api('Main', comment.reply, comment_text)
+				balance -= amount
+				tips = tips_remaining(balance)
+				with open('userlist.txt', 'a') as userlist:
+					userlist.write("%s\n" % author.name)
 
-			if not has_praw_users and has_praw and balance >= amount and comment.id not in open('already_done.txt').read():
-				if author.name in open('userlist.txt').read():
-					log('info', "User %s have already received tip!" % author.name)
+				log('info', "Posted comment. Balance: %.1f Enough for %.0f tips, one tip is %.1f doge" % (balance, tips, amount))
 
-				elif check_parent(comment.parent_id, comment.link_id):
-					log('info', "User %s commented a comment from the bot!" % author.name) 
-
-				else:
-					comment.reply(comment_text)
-					balance -= amount
-					tips = tips_remaining(balance)
-					with open('userlist.txt', 'a') as userlist:
-						userlist.write("%s\n" % author.name)
-
-					log('info', "Posted comment. Balance: %.1f Enough for %.0f tips, one tip is %.1f doge" % (balance, tips, amount))
-	
-				with open('already_done.txt', 'a') as already_done:
-					already_done.write("%s\n" % comment.id)
+			with open('already_done.txt', 'a') as already_done:
+				already_done.write("%s\n" % comment.id)
 			
-		# If 200 or more comments parsed, check the balance to account for tips and calculate new tip
-		if (counter >= 600):
-			counter = 0
-			balance = check_balance()
+	# If 200 or more comments parsed, check the balance to account for tips and calculate new tip
+	if (counter >= 600):
+		counter = 0
+		balance = check_balance()
 
-			amount = calculate_tip(balance)
-			comment_text = ("You seem sad, have some doge!\n\n"
-					"+/u/dogetipbot %.1f doge\n\n"
-					"The amount i tip is entirely based on donations!\n\n"
-					"^^I'm ^^a ^^bot ^^built ^^for ^^sad ^^shibes. ^^Please ^^consider ^^donating ^^to ^^keep ^^me ^^running! ^^[Creator](http://www.reddit.com/user/kaare8p/) ^^[GitHub](https://github.com/kaare8p/Dogecoin-tipper)\n" % amount)
+		amount = calculate_tip(balance)
+		comment_text = ("You seem sad, have some doge!\n\n"
+				"+/u/dogetipbot %.1f doge\n\n"
+				"The amount i tip is entirely based on donations!\n\n"
+				"^^I'm ^^a ^^bot ^^built ^^for ^^sad ^^shibes. ^^Please ^^consider ^^donating ^^to ^^keep ^^me ^^running! ^^[Creator](http://www.reddit.com/user/kaare8p/) ^^[GitHub](https://github.com/kaare8p/Dogecoin-tipper)\n" % amount)
 		
-			if balance < amount:
-				log('info', "Exiting due to lack of funds")
-				exit()
+		if balance < amount:
+			log('info', "Exiting due to lack of funds")
+			exit()
 		
-			tips = tips_remaining(balance)
-			log('info', "\tBalance: %.1f Enough for %.0f tips, one tip is %.1f doge" % (balance, tips, amount))
+		tips = tips_remaining(balance)
+		log('info', "\tBalance: %.1f Enough for %.0f tips, one tip is %.1f doge" % (balance, tips, amount))
 
-		check_donations()
-		time.sleep(300)
-	except (HTTPError, ConnectionError, Timeout, timeout, RedirectException) as error:
-		log('warning', "Main::Reddit is down (%s): Sleeping" % error)
-		time.sleep(30)
-		pass
-        except RateLimitExceeded as error:
-                log('warning', "Main::RateLimitExceeded, sleeping %d seocnds" % error.sleep_time)
-                time.sleep(error.sleep_time)
-                pass
-	except Exception as error:
-		log('warning', "Main::Error: %s" % error)
-		raise
+	check_donations()
+	time.sleep(300)
