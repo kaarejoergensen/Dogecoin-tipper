2de4be227c8f23339632bc3d42b0ceaca87d7854
kaare8p <kaare1994@gmail.com>
2014-06-28 20:50:53 +0200

    Changed error handling again because it clearly doesn't work in a function

--- Dogecoin-tipper.py
+++ Dogecoin-tipper.py
@@ -27,24 +27,6 @@ def log(level, msg):
 	else:
 		logging.info("%s:  %s" % (time.ctime(), msg))
 
-# Function for api-calls
-def api(level, func, *args, **kwargs):
-	while True:
-		try:
-			return func(*args, **kwargs)
-			break
-		except (HTTPError, ConnectionError, RedirectException, Timeout, timeout) as error:
-			log('warning', "%s::Reddit is down (%s): Sleeping" % (level, error))
-			time.sleep(30)
-			pass
-		except RateLimitExceeded as error:
-	                log('warning', "%s::RateLimitExceeded, sleeping %d seconds" % (level, error.sleep_time))
-	                time.sleep(error.sleep_time)
-	                pass
-		except Exception as error:
-			log('warning', "%s::Error: %s" % (level, error))
-			raise
-
 # Update comment text with new amount
 def update_comment(amount): 
 	return ("You seem sad, have some doge!\n\n"
@@ -81,78 +63,130 @@ def tips_remaining(balance):
 
 # Check how many doge is left on the bots account
 def check_balance(send_message):
-	if send_message:
-		api('check_balance()', r.send_message, 'dogetipbot', 'history', '+history')
-        messages = api('check_balance()', r.get_inbox)
-	
-	for message in messages:
-		op_subject = message.subject
+        result = None
+        while result is None:
+                try:
+                        if send_message:
+                                r.send_message('dogetipbot', 'history', '+history')
+                        messages = r.get_inbox()
                         
-		if op_subject == 're: history':
-      			op_text = message.body
-              		op_line = op_text.splitlines()[3]
-	                result=''.join(i for i in op_line if i.isdigit() or i == ".")
-                                
-		        return float(result)
+                        for message in messages:
+                                op_subject = message.subject
+                                        
+                                if op_subject == 're: history':
+                                        op_text = message.body
+                                        op_line = op_text.splitlines()[3]
+                                        result=''.join(i for i in op_line if i.isdigit() or i == ".")
+                                                
+                                        return float(result)
+                except (HTTPError, ConnectionError, RedirectException, Timeout, timeout) as error:
+                        log('warning', "Check_balance()::Reddit is down (%s): Sleeping" % error)
+                        time.sleep(30)
+                        pass
+                except RateLimitExceeded as error:
+        	        log('warning', "Check_balance()::RateLimitExceeded, sleeping %d seconds" % error.sleep_time)
+                        time.sleep(error.sleep_time)
+                        pass
+                except Exception as error:
+        		log('warning', "Check_balance()::Error: %s" % error)
+                        raise
+
 
 # Check for donations and unsubscribe requests
 def check_messages():
-	counter = 0
-	prawTerms = ['+/u/dogetipbot']
-        messages = api('check_messages()', r.get_inbox)
-                
-        for message in messages:
-		op_text = message.body
-		op_subject = message.subject
-		op_author = message.author
+        try:
+                counter = 0
+                prawTerms = ['+/u/dogetipbot']
+                messages = r.get_inbox(limit = 100)
+                        
+                for message in messages:
+                        op_text = message.body
+                        op_subject = message.subject
+                        op_author = message.author
 
-                has_praw = any(string in op_text for string in prawTerms)
+                        has_praw = any(string in op_text for string in prawTerms)
 
-		# If the message is a donation
-                if has_praw and op_author.name != 'dogetipbot' and message.id not in open('already_done.txt').read():
-	                with open('already_done.txt', 'a') as already_done:
-				already_done.write("%s\n" % message.id)
-                	api('check_donations()', message.reply, 'Thank you for tipping! This will help me cheer up other shibes, and will raise the amount I tip! very generosity')
-			counter += 1
-                                
-                       	log('info', "Posted reply to a donation")
+                        # If the message is a donation
+                        if has_praw and op_author.name != 'dogetipbot' and message.id not in open('already_done.txt').read():
+                                with open('already_done.txt', 'a') as already_done:
+                                        already_done.write("%s\n" % message.id)
+                                message.reply('Thank you for tipping! This will help me cheer up other shibes, and will raise the amount I tip! very generosity')
+                                counter += 1
+                                        
+                                log('info', "Posted reply to a donation")
 
-		# If the message is an unsubscribe request
-		if op_subject == 'unsubscribe' and message.id not in open('already_done.txt').read():
-			if op_text == '+comments' or op_text == '+unsubscribe':
-				with open('unsubscribe_comments.txt', 'a') as unsubscribe:
-					unsubscribe.write("%s\n" % op_author.name)
-				api('check_unsubscribe()', message.reply, 'You have been unsubscribed from the bot, all comments made by you will be ignored.')
+                        # If the message is an unsubscribe request
+                        if op_subject == 'unsubscribe' and message.id not in open('already_done.txt').read():
+                                if op_text == '+comments' or op_text == '+unsubscribe':
+                                        with open('unsubscribe_comments.txt', 'a') as unsubscribe:
+                                                unsubscribe.write("%s\n" % op_author.name)
+                                        message.reply('You have been unsubscribed from the bot, all comments made by you will be ignored.')
 
-				log('info', "Unsubscribed user %s from comments" % op_author.name)
-		
-			elif op_text == '+threads':
-				with open('unsubscribe_threads.txt', 'a') as unsubscribe:
-					unsubscribe.write("%s\n" % op_author.name)
-				api('check_unsubscribe()', message.reply, 'You have been unsubscribed from the bot, all comments in threads made by you will be ignored.')
+                                        log('info', "Unsubscribed user %s from comments" % op_author.name)
+                        
+                                elif op_text == '+threads':
+                                        with open('unsubscribe_threads.txt', 'a') as unsubscribe:
+                                                unsubscribe.write("%s\n" % op_author.name)
+                                        message.reply('You have been unsubscribed from the bot, all comments in threads made by you will be ignored.')
 
-				log('info', "Unsubscribed user %s from threads" % op_author.name)
+                                        log('info', "Unsubscribed user %s from threads" % op_author.name)
 
-			with open('already_done.txt', 'a') as already_done:
-					already_done.write("%s\n" % message.id)
-	return counter
+                                with open('already_done.txt', 'a') as already_done:
+                                                already_done.write("%s\n" % message.id)
+
+        except (HTTPError, ConnectionError, RedirectException, Timeout, timeout) as error:
+                log('warning', "Check_messages()::Reddit is down (%s): Sleeping" % error)
+                time.sleep(30)
+                pass
+        except RateLimitExceeded as error:
+        	log('warning', "Check_messages()::RateLimitExceeded, sleeping %d seconds" % error.sleep_time)
+                time.sleep(error.sleep_time)
+                pass
+        except Exception as error:
+        	log('warning', "Check_messages()::Error: %s" % error)
+                raise
+        return counter
 
 #Do not tip replies to own comments
 def check_parent(parent_id, link_id):
-	if parent_id != link_id:
-        	parent = api('check_parent()', r.get_info, thing_id=parent_id)
-                op_author = parent.author
-                if op_author.name == user:
-	                return True
+        try:
+                if parent_id != link_id:
+                        parent = r.get_info(thing_id=parent_id)
+                        op_author = parent.author
+                        if op_author.name == user:
+                                return True
+        except (HTTPError, ConnectionError, RedirectException, Timeout, timeout) as error:
+                log('warning', "Check_parent()::Reddit is down (%s): Sleeping" % error)
+                time.sleep(30)
+                pass
+        except RateLimitExceeded as error:
+        	log('warning', "Check_parent()::RateLimitExceeded, sleeping %d seconds" % error.sleep_time)
+                time.sleep(error.sleep_time)
+                pass
+        except Exception as error:
+        	log('warning', "Check_parent()::Error: %s" % error)
+                raise
 	return False
 
 # Do not tip if author of link is unsubscribed
 def check_op(link_id):
-	submission = api('check_op()', r.get_info, thing_id = link_id)
-	op_author = submission.author.name
+        try:
+                submission = r.get_info(thing_id = link_id)
+                op_author = submission.author.name
 
-	if op_author in open('unsubscribe_threads.txt').read():
-		return True
+                if op_author in open('unsubscribe_threads.txt').read():
+                        return True
+        except (HTTPError, ConnectionError, RedirectException, Timeout, timeout) as error:
+                log('warning', "Check_op()::Reddit is down (%s): Sleeping" % error)
+                time.sleep(30)
+                pass
+        except RateLimitExceeded as error:
+        	log('warning', "Check_op()::RateLimitExceeded, sleeping %d seconds" % error.sleep_time)
+                time.sleep(error.sleep_time)
+                pass
+        except Exception as error:
+        	log('warning', "Check_op()::Error: %s" % error)
+                raise
 	return False
 
 # Update balance, sends message to dogetipbot if message is 1
@@ -176,62 +210,75 @@ comment_text = update_comment(amount)
 
 # Main loop
 while True:
-	comments = api('Main', subreddit.get_comments, limit = 100)
-	# Check for sad comments, and tip 'amount' doge if found
-	for comment in comments:
-		op_author = comment.author
-		op_text = comment.body.lower()
+        try:
+                comments = subreddit.get_comments(limit = 100)
+                # Check for sad comments, and tip 'amount' doge if found
+                for comment in comments:
+                        op_author = comment.author
+                        op_text = comment.body.lower()
 
-		has_praw = any(string in op_text for string in prawWords)
+                        has_praw = any(string in op_text for string in prawWords)
 
-		if has_praw and balance >= amount and comment.id not in open('already_done.txt').read() and op_text != ':(':
-			# Deny if user has already received tip
-			if op_author.name in open('userlist.txt').read():
-				log('info', "User %s already received tip!" % op_author.name)
+                        if has_praw and balance >= amount and comment.id not in open('already_done.txt').read() and op_text != ':(':
+                                # Deny if user has already received tip
+                                if op_author.name in open('userlist.txt').read():
+                                        log('info', "User %s already received tip!" % op_author.name)
 
-			# Deny if user is unsubscribed
-			elif op_author.name in open('unsubscribe_comments.txt').read():
-				log('info', "User %s is unsubscribed from comments!" % op_author.name)
+                                # Deny if user is unsubscribed
+                                elif op_author.name in open('unsubscribe_comments.txt').read():
+                                        log('info', "User %s is unsubscribed from comments!" % op_author.name)
 
-			# Deny if user commented a comment from the bot
-			elif check_parent(comment.parent_id, comment.link_id):
-				log('info', "User %s commented a comment from the bot!" % op_author.name) 
+                                # Deny if user commented a comment from the bot
+                                elif check_parent(comment.parent_id, comment.link_id):
+                                        log('info', "User %s commented a comment from the bot!" % op_author.name) 
 
-			# Deny if op is unsubscribed
-			elif check_op(comment.link_id):
-				log('info', "User %s commented on a thread whre op is unsubscribed!" % op_author.name)
+                                # Deny if op is unsubscribed
+                                elif check_op(comment.link_id):
+                                        log('info', "User %s commented on a thread whre op is unsubscribed!" % op_author.name)
 
-			# Else tip the user
-			else:
-				api('Main', comment.reply, comment_text)
-				balance -= amount
-				tips = tips_remaining(balance)
-				with open('userlist.txt', 'a') as userlist:
-					userlist.write("%s\n" % op_author.name)
+                                # Else tip the user
+                                else:
+                                        comment.reply(comment_text)
+                                        balance -= amount
+                                        tips = tips_remaining(balance)
+                                        with open('userlist.txt', 'a') as userlist:
+                                                userlist.write("%s\n" % op_author.name)
 
-				log('info', "Posted comment. Balance: %.1f Enough for %.0f tips, one tip is %.1f doge" % (balance, tips, amount))
+                                        log('info', "Posted comment. Balance: %.1f Enough for %.0f tips, one tip is %.1f doge" % (balance, tips, amount))
 
-			with open('already_done.txt', 'a') as already_done:
-				already_done.write("%s\n" % comment.id)
-		
-	# If donation received, check the balance to account for it
-	if (check_messages() != 0):
-		balance = update_balance(1)
-		check_balance_time = time.time()
-	
-	# If it's more than 10 minutes since +history message sent, check balance again
-	if time.time() - check_balance_time > 600:
-		balance = update_balance(0)
-		check_balance_time = 0
-		
-	# If 500 or more users in userlist.txt delete file
-	if sum(1 for line in open('userlist.txt')) > 500:
-		open('userlist.txt', 'w').close()
+                                with open('already_done.txt', 'a') as already_done:
+                                        already_done.write("%s\n" % comment.id)
+                        
+                # If donation received, check the balance to account for it
+                if (check_messages() != 0):
+                        balance = update_balance(1)
+                        check_balance_time = time.time()
+                
+                # If it's more than 10 minutes since +history message sent, check balance again
+                if check_balance_time and time.time() - check_balance_time > 600:
+                        balance = update_balance(0)
+                        check_balance_time = 0
+                        
+                # If 500 or more users in userlist.txt delete file
+                if sum(1 for line in open('userlist.txt')) > 500:
+                        open('userlist.txt', 'w').close()
 
-		log('info', "Userlist.txt reset")
+                        log('info', "Userlist.txt reset")
 
-	# Calculate new tip
-	amount = calculate_tip(balance)
-	comment_text = update_comment(amount)	
+                # Calculate new tip
+                amount = calculate_tip(balance)
+                comment_text = update_comment(amount)	
 
-	time.sleep(100)
+                time.sleep(100)
+
+        except (HTTPError, ConnectionError, RedirectException, Timeout, timeout) as error:
+		log('warning', "Main()::Reddit is down (%s): Sleeping" % error)
+		time.sleep(30)
+		pass
+	except RateLimitExceeded as error:
+	        log('warning', "Main()::RateLimitExceeded, sleeping %d seconds" % error.sleep_time)
+	        time.sleep(error.sleep_time)
+	        pass
+	except Exception as error:
+		log('warning', "Main()::Error: %s" % error)
+		raise
